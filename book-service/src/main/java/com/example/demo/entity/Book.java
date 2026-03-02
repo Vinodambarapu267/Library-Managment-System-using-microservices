676a486b39368bd3114c6a8730da45a6874f3c67
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-03-02 17:25:35 +0530

    feat(book-service): update the entity with auto generatie of id

--- book-service/src/main/java/com/example/demo/entity/Book.java
+++ book-service/src/main/java/com/example/demo/entity/Book.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +15,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String isbn;
 	private String title;
